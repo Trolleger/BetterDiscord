27ee0c96cbd5ea909adbd0da10b61f2545e1d154
Andrew_AKA_Trolleger <trolleger2@gmail.com>
2024-12-01 21:34:22 -0600

    first commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    // You can add JavaScript logic here if needed
+  </script>
+  
+  <style>
+    /* Your styles can go here if you'd like to add any */
+  </style>
+  
+  <main>
+    <h1>~~~ This Is My Homepage For All My Stuff, Hope You Enjoy -Trolleger ~~~</h1>
+    
+    
+    <p>Here are some websites I made</p>
+
+    <p>My Youtube Channels</p>
+  </main>
+  
+  <nav>
+    <a href="/Youtube_Things">My Youtube Stuff!</a>
+  </nav>
---
+++ my-app/src/routes/Youtube_Things/Variety_Gaming_Channel/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    // Your script logic goes here
+  </script>
+  
+  <style>
+    /* Your component-specific styles go here */
+  </style>
+  
+  <main>
+    <h1>Welcome to the page!</h1>
+    <p>This is the content of your page. Customize it as needed!</p>
+  </main>
+  
