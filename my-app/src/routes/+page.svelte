9414134721652e664e9e416c628f232906fd6df8
Andrew_AKA_Trolleger <trolleger2@gmail.com>
2024-12-03 09:18:56 -0500

    more homepage and yt stuff

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -5,8 +5,10 @@
   <style>
     h1 {
       font-size: large;
-
+      color: blue;
+      text-align: center;
     }
+    
     /* Your styles can go here if you'd like to add any */
   </style>
   
@@ -14,20 +16,28 @@
     <h1>~~~ This Is My Homepage For All My Stuff, Hope You Enjoy -Trolleger ~~~</h1>
     
     <p>
-      Hello! my name is Andrew, I am a student who is interested in many things. I am always ready to further my education, businesses and my self!
+      Hello! my name is Andrew, I am a student who is interested in many things. I am always ready to further my education, businesses and improve my self!
       Down below are some of my certifications, things I have done, coding things, content creation things :/ And more!
     </p>
-    
+    <br>
+    <br>
     <p>Here are some websites I made</p>
+    <br>
 
-    <p>My Youtube Channels</p>
+    <p>Down below are my Youtube Channels!</p> 
     
     <nav>
-      <a href="/Youtube_Things">My Youtube Stuff!</a>
+      <span>
+      <a href="/Youtube_Things/Main_Minecraft_Channel">My Minecraft Channel!</a>
+      <a href="/Youtube_Things/Conspiracy_And_Random_Channel">My Conspiracy And Random Channel!</a>
+      <a href="/Youtube_Things/Variety_Gaming_Channel">My Variety Gaming Channel!</a> 
+    </span>
     </nav>
 
   </main>
-  
+  <!-- AN IDEA INSTEAD OF HAVING A YOUTUBE THINGS JUST HAVE AN IMAGE CIRCLE THEN THEY CAN PRESS ON THE THINGS AND GET MORE INFORMATION -->
+
+
 <!-- IF YOU WANT TO PUSH, THE COMMANDS ARE
  git add .
  git commit -m "(what ever message you pick)" 
