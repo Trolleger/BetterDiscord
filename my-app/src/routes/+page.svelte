b5827def68e0ebc211396a0a1a39a54f4e5782a5
Andrew_AKA_Trolleger <trolleger2@gmail.com>
2024-12-03 09:07:09 -0500

    some more informations about me

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -3,12 +3,20 @@
   </script>
   
   <style>
+    h1 {
+      font-size: large;
+
+    }
     /* Your styles can go here if you'd like to add any */
   </style>
   
   <main>
     <h1>~~~ This Is My Homepage For All My Stuff, Hope You Enjoy -Trolleger ~~~</h1>
     
+    <p>
+      Hello! my name is Andrew, I am a student who is interested in many things. I am always ready to further my education, businesses and my self!
+      Down below are some of my certifications, things I have done, coding things, content creation things :/ And more!
+    </p>
     
     <p>Here are some websites I made</p>
 
@@ -27,5 +35,6 @@
  DO NOT USE NORMAL WINDOWS SETTING THINGY THAT STUFF DONT WORK.
  To run the server do:
  cd my-app
- npm run dev-->
+ npm run dev
+ AND WE GOT IT-->
  
