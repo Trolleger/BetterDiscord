b176a5051dbafe2cd76bc05d56754de98c5231dd
Andrew_AKA_Trolleger <trolleger2@gmail.com>
2024-12-05 13:00:04 -0500

    battery running low; added to minecraft gaming channel n other stuff

--- my-app/src/routes/Youtube_Things/Main_Minecraft_Channel/+page.svelte
+++ my-app/src/routes/Youtube_Things/Main_Minecraft_Channel/+page.svelte
@@ -3,11 +3,20 @@
   </script>
   
   <style>
-    /* Your component-specific styles go here */
+    main{
+      background-image:-webkit-linear-gradient(black,blue)
+    }
   </style>
   
   <main>
-    <h1>Welcome to the page!</h1>
-    <p>This is the content of your page. Customize it as needed!</p>
+    <span>
+    <h1 style="font-size: 6em; text-align: center; color:maroon; font-family: Consolas, Monaco, monospace;">This is my Minecraft Channel</h1>
+    </span>
+    <p style="font-size: 3em; color: white  "> Well, this was my first and main channel. Before it I had another youtube channel, but I unfortuantley lost the Gmail to it randomly one day because google felt like they wanted to lock me out for "Security purposes"
+      But thats besides the point, originally this was my channel for some random content I posted, be it roams.io/bettermope.io/mohio or whatever you call it now, roblox, memes, ecetera. But I decided to do minecraft for a change.
+      But my first videos did horribly, then the next, then the next, but I kept on perservering and I finally started getting into the hundreds of views! I started posting shorts and unlisted all the irrevalent content on my youtube
+      channel. And today I am doing very good working on new videos and stuff! As always this is a minecraft first youtube channel So thats where I am right now, ill report back when I get 1000 subscribers :)
+    </p>
+ 
   </main>
   
