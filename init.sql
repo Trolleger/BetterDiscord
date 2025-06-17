5179ea595db5e577257af8b686fae3be50dfc746
Andrew <trolleger2@gmail.com>
2025-06-17 12:54:08 -0400

    moved stuff from db-init to init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,11 @@
+-- Database initialization script
+-- This script will be executed by the db-init container
+
+-- Test connection first
+SELECT 1 AS connection_test;
+
+-- Create database and users
 CREATE DATABASE IF NOT EXISTS chat_app_dev;
+CREATE USER IF NOT EXISTS craig WITH PASSWORD 'cockroach12938';
+GRANT ALL ON DATABASE chat_app_dev TO craig;
+GRANT ADMIN TO craig;
